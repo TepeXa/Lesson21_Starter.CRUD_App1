e1b484d43b301041efdef22f585d74e0c582bc1f
Terentyev-DA <tepexa@gmail.com>
2022-08-03 18:35:16 +0500

    Alishev lesson 23

--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -31,5 +31,11 @@ public class PersonDAO {
 
     }
 
-
+    public void update(int id, Person updatePerson) {
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(updatePerson.getName());
+    }
+    public void delete(int id) {
+        people.removeIf(p->p.getId()==id);
+    }
 }
